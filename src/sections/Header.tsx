ea3ccf78f74171ca01021090f9e2e88682249210
Vijay Patel <148934914+VIJAY7488@users.noreply.github.com>
2025-02-03 11:38:15 +0530

    Update Header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,16 +1,26 @@
 
 
 export const Header = () => {
+
+  const handlescrooll = (id) => {
+    const section = document.getElementById(id);
+
+    if(section){
+      section.scrollIntoView({behavior: 'smooth'})
+    }
+  };
+
+
   return (
    <div className="flex justify-center items-center fixed w-full z-10 top-3">
         <nav 
         className='flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur'>
-        <a href="/home" 
-        className='nav-item'>Home</a>
-        <a href="/projects" className='nav-item'>Projects</a>
-        <a href="/about" className='nav-item'>About</a>
-        <a href="/contact" 
-        className='nav-item bg-white text-gray-900'>Contact</a>
+        <button onClick={() => handlescrooll('home')} 
+        className='nav-item'>Home</button>
+        <button onClick={() => handlescrooll('projects')} className='nav-item'>Projects</button>
+        <button onClick={() => handlescrooll('about')} className='nav-item'>About</button>
+        <button onClick={() => handlescrooll('contact')} 
+        className='nav-item bg-white text-gray-900'>Contact</button>
       </nav>
    </div>
   )
